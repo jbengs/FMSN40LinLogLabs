cfa4322843ed096acc6a2065b3840896d2e2cbfa
Joel Bengs <joel.bengs@gmail.com>
2022-03-30 17:03:30 +0200

    work on 1bc

--- R/Lab1B.R
+++ R/Lab1B.R
@@ -12,5 +12,28 @@ ggplot(data = Pb_Jamtland, aes(x = year, y = log(Pb))) + geom_point()
 
 # 1.B(c). Fit this log-transformed linear model and calculate the β-estimates,
 #their standard errors and 95 % confidence intervals.
+# 1 - get the model using lm
+# 2 - create a complete dataframe with original data, the model and intervals
+# 3 - remove duplicates
+loglin <- lm(log(Pb) ~ year, data = Pb_Jamtland)
 
-loglin = lm(log(Pb) ~ year, data = Pb_Jamtland)
+(estimates <- cbind(
+  beta = loglin$coefficients,
+  confint(loglin)
+))
+
+loglin.summary <- summary(loglin)
+coefficients <- loglin.summary$coefficients
+stderror <- coefficients["Std. Error"]
+
+loglin.dataset <- cbind(
+  Pb_Jamtland,
+  fit = predict(loglin),
+  conf = predict(loglin, interval = "confidence"),
+  pred = predict(loglin, interval = "predict")
+)
+
+loglin.dataset$conf.fit <- NULL
+loglin.dataset$pred.fit <- NULL
+
+
