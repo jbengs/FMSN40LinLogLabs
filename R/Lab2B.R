ba28b8bdc43713039f38831cdc8a5ee6d149f28b
Joel Bengs <joel.bengs@gmail.com>
2022-04-03 15:54:13 +0200

    work on lab2b

--- R/Lab2B.R
+++ R/Lab2B.R
@@ -33,6 +33,60 @@ ggplot(data = Pb.pred , aes(x = year, y = log(Pb))) +
   
 
 
-#2.B(c). Fit a model using lm(log(Pb) ~ I(year - 1975) + region, data = Pb_all)
+#2.B(c). Fit a model using lm(log(Pb) ~ I(year - 1975) + region, data = Pb_all) ####
 # Which location was used as reference? Is this a good idea?
-  
+# Answer: ÖREBRO was used as it is the first one. Not good as Örebro has the least number of observations
+(summary(Pb_all))
+loglin.region <- lm(log(Pb) ~ year + region, data = Pb_all)
+(summary(loglin.region)$coefficients)
+
+
+# 2.B(d). The reference category should always be a large category. Change the region variable so ####
+# that [mzq]= Jamtland will be used as reference instead.
+Pb_all$region <- relevel(Pb_all$region, "Jamtland")
+# Then refit the model in 2.B(c).
+loglin.region <- lm(log(Pb) ~ year + region, data = Pb_all)
+(exp(summary(loglin.region)$coefficients))
+   
+# How high was the average concentration of lead in region [mzq] = Jamtland in 1975, with CI, according to themodel?
+#ANSWER:  The exponential of the intercept plus the betafactor for that region. But now Jamtland is reference, so it is incorporated in B0 (the interecept)
+(exp(confint(loglin.region)))
+#How fast is the rate of decrease in lead concentration, according to this model? ANSWER: exp(B1) = exp(Betayear)
+
+# 2.B(e). ####
+#Use the model to calculate a 95 % confidence interval and a 95 % prediction interval for
+#the lead concentration (mg/kg) in region [mzq] = Jamtland in the year [mzq. = 2014
+
+Jamtland.2014 <- data.frame(year = c(2014-1975), region = "Jamtland")
+Jamtland.2014.pred <- cbind(
+  Jamtland.2014,
+  fit = exp(predict(loglin.region, Jamtland.2014)),
+  conf = exp(predict(loglin.region, Jamtland.2014, interval = "confidence")),
+  pred = exp(predict(loglin.region, Jamtland.2014, interval = "prediction"))
+)
+Jamtland.2014.pred$conf.fit <- Jamtland.2014.pred$pred.fit <- NULL
+(Jamtland.2014.pred)
+
+
+
+# 2.B(f ). ####
+#Estimate the ratio between the expected Pb-level in Orebro and the level in Jamtland for any given year.
+
+
+
+
+
+
+
+Pb.region.pred <- cbind(
+  Pb_all,
+  fit = predict(loglin.region),
+  conf = predict(loglin.region, interval = "confidence"),
+  pred = predict(loglin.region, interval = "prediction")
+)
+Pb.region.pred$conf.fit <- Pb.region.pred$pred.fit <- NULL
+
+ggplot(data = Pb.region.pred , aes(x = year, y = log(Pb))) + 
+  geom_point() + 
+  facet_wrap(~region) +
+  geom_line(aes(y = fit))
